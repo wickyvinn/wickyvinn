5976a623b145e78c508b35e4546adcbb15a0bccb
wickyvinn <vicky.winn@gmail.com>
2015-06-08 17:23:27 -0700

    Row up icons for portfolio page.

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.render('home.html');
+});
+
+app.get('/portfolio', function(req, res) {
+  res.render('portfolio.html');
 });
 
 app.listen(app.get('port'), function() {
